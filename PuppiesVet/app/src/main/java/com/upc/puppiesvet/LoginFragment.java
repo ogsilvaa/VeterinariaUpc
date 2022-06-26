c32128067b3c155718c090bd66894cfede0fa72c
Alonso <alonso_vento@outlook.com>
2022-06-26 02:01:47 -0500

    Diseño de Fragmento LOGIN

--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/LoginFragment.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/LoginFragment.java
@@ -2,11 +2,15 @@ package com.upc.puppiesvet;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class LoginFragment extends Fragment {
@@ -18,5 +22,15 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btn_Ingresar = view.findViewById(R.id.btn_Ingresar);
+        btn_Ingresar.setOnClickListener(v -> {
+            NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.inicioFragment);
+        });
+
     }
 }
