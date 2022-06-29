4147dd03682602c13c4c86aea5ac56baeea5e275
Alonso <alonso_vento@outlook.com>
2022-06-29 18:03:10 -0500

    Se implementó formulario Registro Mascota

---
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/ui/mascota/RegistrarMascotasFragment.java
@@ -0,0 +1,23 @@
+package com.upc.puppiesvet.ui.mascota;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.upc.puppiesvet.R;
+
+
+public class RegistrarMascotasFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registrar_mascotas, container, false);
+    }
+}
