9d8a768b9ab2bdf1523e22ce15202b926a6b8458
Alonso <alonso_vento@outlook.com>
2022-06-29 14:42:03 -0500

    Se implementó layout INICIO como fragmento - Navegación ok

---
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/ui/navheader/InicioFragment.java
@@ -0,0 +1,24 @@
+package com.upc.puppiesvet.ui.navheader;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.upc.puppiesvet.R;
+
+
+public class InicioFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+}
