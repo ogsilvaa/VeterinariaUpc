931b50301a268f0ecaa793d374e353a0f58a2e49
Alonso <alonso_vento@outlook.com>
2022-06-30 01:35:48 -0500

    Primer Paso de Asignacion de Referencias de Perfil Activity - Funcionamiento de Botón

--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/PerfilActivity.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/PerfilActivity.java
@@ -2,13 +2,30 @@ package com.upc.puppiesvet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    Button btn_RegistrarPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+        asignarReferencias();
+    }
+
+    private void asignarReferencias() {
+        btn_RegistrarPerfil=findViewById(R.id.btn_RegistrarPerfil);
+        btn_RegistrarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(PerfilActivity.this,PerfilActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
