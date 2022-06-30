60e9a8c9d72c6900e70e3450b6d9877d9868e5e3
Alonso <alonso_vento@outlook.com>
2022-06-29 22:20:52 -0500

    Se crea realiza configuración de firebase en el proyecto

--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/MainActivity.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/MainActivity.java
@@ -2,13 +2,22 @@ package com.upc.puppiesvet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.upc.puppiesvet.ui.perfil.PerfilFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/ui/perfil/PerfilFragment.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/ui/perfil/PerfilFragment.java
@@ -7,18 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.upc.puppiesvet.R;
 
 
 public class PerfilFragment extends Fragment {
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
+
     }
+
+
 }
