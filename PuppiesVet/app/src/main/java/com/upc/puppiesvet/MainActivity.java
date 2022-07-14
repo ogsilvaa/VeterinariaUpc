cfad8affb66281efb95ee27eea8d05bb17ed24e1
Alonso <alonso_vento@outlook.com>
2022-07-14 00:31:42 -0500

    Se implementa Firebase Cloud Messaging de forma exitosa

--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/MainActivity.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/MainActivity.java
@@ -1,24 +1,33 @@
 package com.upc.puppiesvet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_Bienvenida;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         btn_Bienvenida=(Button)findViewById(R.id.btn_Bienvenida);
         btn_Bienvenida.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /* Para obtener el token de la aplicación ::: Abrir el logcat y poner el símbolo indicado en el TAG*/
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                String token = task.getResult();
+                Log.d("===>",token);
+            }
+        });
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        database= FirebaseDatabase.getInstance();
+        reference =database.getReference();
     }
 }
