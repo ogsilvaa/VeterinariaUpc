c910686f1b18f204d5fb2360e695a859f8100745
Alonso <alonso_vento@outlook.com>
2022-07-15 00:45:36 -0500

    Se implementa Servicios Activity

--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/InicioActivity.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/InicioActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class InicioActivity extends AppCompatActivity {
 
-    Button btn_Perfil, btn_Mascota;
+    Button btn_Perfil, btn_Mascota, btn_Servicios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,7 @@ public class InicioActivity extends AppCompatActivity {
 
         btn_Perfil=(Button)findViewById(R.id.btn_Perfil);
         btn_Mascota=(Button)findViewById(R.id.btn_Mascota);
+        btn_Servicios=(Button)findViewById(R.id.btn_Servicios);
 
         btn_Perfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +35,14 @@ public class InicioActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btn_Servicios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicioActivity.this, ServiciosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
