314d466655b26397c6316ccd622abfb98655c14b
Alonso <alonso_vento@outlook.com>
2022-07-15 02:59:58 -0500

    Se implementa MapActivity y se configura clave API en Manifest

--- PuppiesVet/app/src/main/java/com/upc/puppiesvet/InicioActivity.java
+++ PuppiesVet/app/src/main/java/com/upc/puppiesvet/InicioActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class InicioActivity extends AppCompatActivity {
 
-    Button btn_Perfil, btn_Mascota, btn_Servicios;
+    Button btn_Perfil, btn_Mascota, btn_Servicios, btn_Map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +43,13 @@ public class InicioActivity extends AppCompatActivity {
             }
         });
 
-
+        btn_Map=(Button)findViewById(R.id.btn_Map);
+        btn_Map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicioActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
